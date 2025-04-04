e87740242472a2f887b6a2a4997bc341ffd1e472
dubeycode <dubeysatyam3635@gmail.com>
2025-04-04 19:18:31 +0530

    Add files via upload

--- Array/twosum.cpp
+++ Array/twosum.cpp
@@ -15,9 +15,9 @@ int main(){
     }
 
     int target;
+     cout<<"Enter the target sum:";
+     cin>>target;
 
-    cout<<"Enter the target sum:";
-    cin>>target;
     for(int i=0;i<n-1;i++){
      for(int j=i+1;j<n;j++){
         if(arr[i]+arr[j]== target){
@@ -145,8 +145,10 @@ int main(){
     for(int i=0;i<n-1;i++){
         cin>>arr[i];
     }
+
     cout<<"Enter the target value:";
    cin>>target;
+   
    bool found=false;
    //burte force approch
    //The first person shakes hands with everyone after them.  
